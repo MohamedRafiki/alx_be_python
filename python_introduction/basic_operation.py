108394d9349e6b218a32124698a1211118d3134e
MohamedRafiki <rafiki0105@outlook.com>
2024-06-23 12:49:06 -0700

    basic_operations.py done

---
+++ python_introduction/basic_operation.py
@@ -0,0 +1,11 @@
+#define variables
+number1 = 10
+number2 = 5
+#calculate the sum,difference and multiplication
+sum_result = number1 + number2
+difference_result = number1 - number2
+multiplication_result = number1 * number2
+#print the result 
+print(f"sum of {number1} and {number2} is {sum_result}")
+print(f"difference of {number1} and {number2} is {difference_result}")
+print(f"multiplication of {number1} and {number2} is {multiplication_result}")
