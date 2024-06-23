66f75b5b856100463757ad9dae993b6ab21642d4
MohamedRafiki <rafiki0105@outlook.com>
2024-06-23 13:36:01 -0700

    interest calculator

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+#define variables
+principal = 1000
+rate = 0.05
+time = 3
+#interest function
+interest = principal * rate * time
+#calcul interest
+print(f"The simple interest is: {interest}")
+
+
